dd9954e5ecd1cb60983cbbeb367fe4f848ff4830
Stanislav Grigorevskiy <sgrigorevskiy@eccentex.com>
2022-01-13 18:07:49 +0200

    error handling

--- app.js
+++ app.js
@@ -80,7 +80,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error', {
-    title: err.message
+    errormessage: err.message,
+    url: req.url,
+    method: req.method,
+    status: err.status
   });
 });
 
